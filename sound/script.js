7ec2ee89056f4ae14a0486a7a0eb9671e763d4ed
PrincessCoder <72946882+PrincessCoder@users.noreply.github.com>
2023-09-19 23:18:34 -0500

    script update

--- sound/script.js
+++ sound/script.js
@@ -1,4 +1,4 @@
-let mic; 
+let audio1 = mic
 const audioCtx = new AudioContext();
 console.log(audioCtx);
 
@@ -6,3 +6,23 @@ const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+    {
+    // Create an Audio input
+    mic = new p5.AudioIn();
+  
+    // start the Audio Input.
+    // By default, it does not .connect() (to the computer speakers)
+    mic.start();
+  }
+  {
+    // Get the overall volume (between 0 and 1.0)
+    let vol = mic.getLevel();
+    fill(0);
+    stroke(200);
+  
+    // Draw an ellipse with height based on volume
+    let h = map(vol, 0, 1, height, 0);
+    ellipse(width / 2, h - 25, 50, 50);
+  }
+  
